3bdda442dbdb1c7460742f563e2db11136abc069
Anmol Tiwari <anmoltiwari147@gmail.com>
2026-07-20 14:20:39 +0530

    Covered time complexity and space complexity

---
+++ day - 5/script.js
@@ -0,0 +1,28 @@
+// time and space complexity
+
+//there are three main time complexity which is constant O(1) liner O(n) quadratic O(n2)
+
+//In which time complexity is constant O(1)
+// let age = 20;
+// console.log(age)
+
+
+//In which time complexity is liner O(n)
+// for (let i = 0; i <n; i++){
+// console.log(i)
+
+
+//In which time complexity is quadratic O(n2)
+
+// for (let i = 0; i < n; i++){
+//   for (let i = 1; i < n; i++){
+//     console.log("ANMOL")
+//   }
+// }
+
+
+
+
+// space complexity - space complexity measures how much extra memory an algorithm uses as the input size (n) grows 
+
+// Space complexity is same like a time complexity
