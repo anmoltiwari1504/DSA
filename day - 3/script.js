8d76feb20ebeeb6a1f7d9a7adda4203746be6c83
Anmol Tiwari <anmoltiwari147@gmail.com>
2026-07-20 14:19:27 +0530

    Revision and debugging day

---
+++ day - 3/script.js
@@ -0,0 +1,38 @@
+//  Armstrong Number
+// function isArmStrong(num) {
+//   let originalNumber = num;
+//   let temp = num;
+//   let count = 0
+//   while (temp > 0) {
+//     count++
+//     temp = Math.floor(temp / 10)
+//   }
+//   let sum = 0;
+//   while (num > 0) {
+//     let digit = num % 10;
+//     sum += Math.pow(digit, count);
+//     num = Math.floor(num /10 )
+//   }
+//  return sum === originalNumber
+// }
+ 
+// console.log(isArmStrong(153))
+
+
+//perfect number
+
+// function isPerfect(num) {
+//   let sum = 0;
+//   for (let i = 1; i < num - 1; i++){
+//     if (num % i === 0) {
+//       sum +=  i;
+//     }
+//   }
+//   return sum === num
+
+  
+// }
+// console.log(isPerfect(6))
+
+
+// Today is debugging and revision day  also so i solve only 2 question 
